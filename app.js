0b66d5c98a8fd145093c4c153dcd1387218d9394
Mohammad <mfalahat2002@gmail.com>
2023-07-15 11:18:09 +0330

    update js file by removing some noisy code

--- app.js
+++ app.js
@@ -1,78 +1,57 @@
 const result = document.getElementById("display");
+result.value = result.value.toString();
 
 
 function showNumber1(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 1;
     else return result.value += '1';
 }
 function showNumber2(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 2;
     else return result.value += '2';
 }
 function showNumber3(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 3;
     else return result.value += '3';
 }
 function showNumber4(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 4;
     else return result.value += '4';
 }
 function showNumber5(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 5;
     else return result.value += '5';
 }
 function showNumber6(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 6;
     else return result.value += '6';
 }
 function showNumber7(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 7;
     else return result.value += '7';
 }
 function showNumber8(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 8;
     else return result.value += '8';
 }
 function showNumber9(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return result.value = 9;
 
     else return result.value += '9';
 }
 function showNumber0(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return;
     else return result.value += 0;
 }
 function showNumber00(){
-    result.value = result.value.toString();
-
     if(result.value.length === 1 && result.value.includes('0')) return;
 
     if (result.value.length === 0) return;
     return result.value += '00';
 }
 function showDotted(){
-    result.value.toString();
     if(result.value.at(-1) === '.') return;
+
     if(
      (result.value.length === 0)||
      (result.value.at(-1) === '*')||
@@ -85,7 +64,6 @@ function showDotted(){
 }
 
 function showTimes(){
-    result.value.toString();
     if(result.value.at(-1) === '*' || result.value.length === 0)
     return;
     else if(
@@ -96,7 +74,6 @@ function showTimes(){
     return result.value += '*';
 }
 function showDivide(){
-    result.value.toString();
     if(result.value.at(-1) === '/' || result.value.length === 0)
     return;
     else if(
@@ -107,7 +84,6 @@ function showDivide(){
     return result.value += '/';
 }
 function showPlus(){
-    result.value.toString();
     if(result.value.at(-1) === '+' || result.value.length === 0)
     return;
     if(
@@ -118,7 +94,6 @@ function showPlus(){
     return result.value += '+';
 }
 function showMinus(){
-    result.value.toString();
     if(result.value.at(-1) === '-' || result.value.length === 0)
     return;
     if(
@@ -132,22 +107,11 @@ function showAllClear(){
     return result.value = '';
 }
 function showDelete(){
-    return result.value = result.value.toString().slice(0, -1);
+    return result.value = result.value.slice(0, -1);
 }
 function showEqual(){
-    result.value = result.value.toString();
     if(result.value.length === 0) return;
 
-    return result.value = eval(result.value);
+    else return result.value = eval(result.value);
 
 }
-
-
-
-
-
-
-
-
-
-
